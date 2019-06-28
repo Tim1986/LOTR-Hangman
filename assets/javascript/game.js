5c79b13f285fe736b1b3f8adcf90b8476cb12f7e
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-06-27 22:16:37 -0400

    finished draft of game, still needs minor tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,9 @@ var currentAnswerDisplayed = document.getElementById("answer")
 var remainingGuesses = document.getElementById("guesses-left")
 var alreadyGuessedSpan = document.getElementById("already-guessed")
 var alreadyGuessed = []
-var answerArray = []
+// var answerArray = []
+var answerArrayHidden = []
+
 
 
 function newWord() {
@@ -15,30 +17,47 @@ function newWord() {
     alreadyGuessedSpan.textContent = "none"
     guesses = 12
     remainingGuesses.textContent = 12;
+    answerArrayHidden = []
 
     var currentAnswer = possibleAnswers[Math.floor(Math.random() * possibleAnswers.length)];
     console.log(currentAnswer)
     console.log(currentAnswer.length)
-    answerArray.push(currentAnswer)
-    console.log(answerArray)
-    currentAnswerDisplayed.textContent = currentAnswer
+    // answerArray.push(currentAnswer)
+    // console.log(answerArray)
 
-    // how do I make this a string of underscores? Do I use .toString()?
+    for (i = 0; i < currentAnswer.length; i++) {
+        answerArrayHidden.push("_")
+    }
+    console.log(answerArrayHidden)
+    currentAnswerDisplayed.textContent = answerArrayHidden
 
         document.onkeyup = function(event) {
             var userGuess = event.key.toLowerCase();
            console.log(userGuess)
-
+            
+            function correctGuess() {
+            var str = answerArrayHidden.toString();
+            var res = str.split(" ");
+            document.getElementById("answer").innerHTML = res
+            answerArrayHidden[j] = userGuess;            
+            }
+        
             if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")) {
                 if (currentAnswer.includes(userGuess)) {
                     console.log("yes");
-                    // somehow replace a corresponding underscore with userGuess
 
-                    // if (missing answers === 0) {
-                        // wins++;
-                        // winsText.textContent = wins;
-                    //     newWord()
-                    // }
+                    for (j = 0; j < currentAnswer.length; j++) {
+                        if (currentAnswer[j] === userGuess) {
+                            console.log(j)
+                            correctGuess()
+                            currentAnswerDisplayed.textContent = answerArrayHidden
+                            }
+                        }
+                        if (answerArrayHidden.includes("_") === false) {
+                            wins++;
+                            winsText.textContent = wins;
+                            newWord()
+                        }
                     
                 } else {
                     if (alreadyGuessed.includes(userGuess) === false) {
@@ -54,10 +73,10 @@ function newWord() {
                     }
                 }
                 
-            
-                }
-                }
                 }
             }
+    }
+}
+            
 
 newWord();
